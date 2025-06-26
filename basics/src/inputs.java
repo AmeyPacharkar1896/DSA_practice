d6a4bad5d50b9c5a8a3f9c7785d22f4834ff577b
Amey Pacharkar <ameypacharkar076@gmail.com>
2025-06-26 20:41:20 +0530

    basics of java

---
+++ basics/src/inputs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class inputs {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your name:");
+    String name = sc.nextLine();
+    System.out.println("Enter your age:");
+    int age = sc.nextInt();
+    System.out.println("Enter your address:");
+    String address = sc.nextLine();
+    System.out.println("Enter your phone number:");
+    String phone = sc.nextLine();
+    System.out.println("Enter your email:");
+    String email = sc.nextLine();
+    System.out.println("Your name is: " + name);
+    System.out.println("Your age is: " + age);
+    System.out.println("Your address is: " + address);
+    System.out.println("Your phone number is: " + phone);
+    System.out.println("Your email is: " + email);
+  }
+}
---
+++ basics/src/primitives.java
@@ -0,0 +1,18 @@
+public class primitives {
+  public static void main(String[] args) {
+    int rollno = 64;
+    char letter = 'r';
+    float marks = 85.5f;
+    double largeDecimalNumber = 278427.424924;
+    long largeInterger = 317842747294L;
+    boolean check = false;
+
+    System.out.println("rollno: " + rollno);
+System.out.println("letter: " + letter);
+System.out.println("marks: " + marks);
+System.out.println("largeDecimalNumber: " + largeDecimalNumber);
+System.out.println("largeInterger: " + largeInterger);
+System.out.println("check: " + check);
+
+  }
+}
